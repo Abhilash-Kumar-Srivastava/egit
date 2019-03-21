8189f8a6dea08142f01a0811094536dd5249e1f8
Abhilash-Kumar-Srivastava <sekher.abhi@gmail.com>
2019-03-21 15:41:32 +0530

    Update EgitMainClass.java
    Commit

--- egitTestProject/src/egitTestProject/EgitMainClass.java
+++ egitTestProject/src/egitTestProject/EgitMainClass.java
@@ -2,11 +2,13 @@ package egitTestProject;
 
 public class EgitMainClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+	for(int i=0; i<5 ;i++) {
 		
-		//Test start
+		System.out.println("Hi Hello");
+		System.out.println("Bye");
 		
-		//Test end
+	}
 
 	}
 
