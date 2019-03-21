191a57ac52cef6a11ad5a1bd777875193d805110
Abhilash-Kumar-Srivastava <sekher.abhi@gmail.com>
2019-03-21 13:55:39 +0530

    Holi
    print Holi 10 times

--- egitTestProject/src/egitTestProject/EgitTest2.java
+++ egitTestProject/src/egitTestProject/EgitTest2.java
@@ -3,7 +3,9 @@ package egitTestProject;
 public class EgitTest2 {
 
 public static void main(String[] args) {
-	
+	for(int i=0; i<10; i++){
+		System.out.println("Happy Holi");
+	}
 	//Test start
 	System.out.println(" greats");
 	//Test end
