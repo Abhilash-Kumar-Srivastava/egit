2d2b0cd615af8500bb87f9157c558d4d27aec8cd
Abhilash-Kumar-Srivastava <sekher.abhi@gmail.com>
2019-03-21 15:55:30 +0530

    Update EgitMainClass.java
    ggggggggg

--- egitTestProject/src/egitTestProject/EgitMainClass.java
+++ egitTestProject/src/egitTestProject/EgitMainClass.java
@@ -3,13 +3,13 @@ package egitTestProject;
 public class EgitMainClass {
 
 	public static void main(String[] args) {		
-	for(int i=0; i<5 ;i++) {
+	
 		
 		System.out.println("Hi Hello");
 		System.out.println("Bye");
 		
 	}
 
-	}
+
 
 }
