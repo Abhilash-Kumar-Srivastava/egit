f17a2b8b41e6fd740250237ff5e79a00a9448dfd
Abhilash.K <Abhilash.K@WKSBAN29RBW1059.corp.mphasis.com>
2019-03-21 15:14:23 +0530

    Testing in progress

--- egitTestProject/src/egitTestProject/EgitTest2.java
+++ egitTestProject/src/egitTestProject/EgitTest2.java
@@ -4,7 +4,7 @@ public class EgitTest2 {
 
 public static void main(String[] args) {
 	
-	System.out.println("Vinilda do not look like an aunt");
+	System.out.println("Vinilda do not look like an aunt nor mother");
 	
 	for(int i=0; i<10; i++){
 		
