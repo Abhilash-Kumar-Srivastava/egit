0a5c441447aeb8729c6548c06a8159b199f3707f
Abhilash.K <Abhilash.K@WKSBAN29RBW1059.corp.mphasis.com>
2019-03-21 15:12:32 +0530

    Testing

--- egitTestProject/src/egitTestProject/EgitTest2.java
+++ egitTestProject/src/egitTestProject/EgitTest2.java
@@ -3,7 +3,12 @@ package egitTestProject;
 public class EgitTest2 {
 
 public static void main(String[] args) {
+	
+	System.out.println("Vinilda do not look like an aunt");
+	
 	for(int i=0; i<10; i++){
+		
+		System.out.println("Nidhie is missing");
 		System.out.println("Happy Holi");
 	}
 	//Test start
