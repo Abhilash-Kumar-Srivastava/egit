5db92db9d6086b3d2ff5f528a2b7eed1cd801b89
Abhilash.K <Abhilash.K@WKSBAN29RBW1059.corp.mphasis.com>
2019-03-21 14:01:36 +0530

    Test

--- egitTestProject/src/egitTestProject/EgitTest2.java
+++ egitTestProject/src/egitTestProject/EgitTest2.java
@@ -4,6 +4,10 @@ public class EgitTest2 {
 
 public static void main(String[] args) {
 	
+	for(int i=0; i<10; i++) {
+		
+		System.out.println("Pagal Vinilda");
+	}
 	//Test start
 	System.out.println(" greats");
 	//Test end
